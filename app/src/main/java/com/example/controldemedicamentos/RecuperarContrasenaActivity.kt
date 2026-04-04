8badc7d75c714e079646dcc547f80fb1329d27ca
ElBladir <bladir.carpio@gmail.com>
2026-04-03 22:54:26 -0700

    pantalla de recuperarcontrasena y texto programado que manda al inicio de sesion junto con la pantalla de seleccion de cuenta de google

---
+++ app/src/main/java/com/example/controldemedicamentos/RecuperarContrasenaActivity.kt
@@ -0,0 +1,28 @@
+package com.example.controldemedicamentos
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class RecuperarContrasenaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Esto es lo que conecta este código con tu diseño bonito
+        setContentView(R.layout.activity_recuperar_contrasena)
+
+        // 1. Enlazamos el texto que funciona como botón de regreso
+        val tvVolverInicio = findViewById<TextView>(R.id.tv_volver_inicio)
+
+        // 2. Le decimos qué hacer cuando lo toquen
+        tvVolverInicio.setOnClickListener {
+            // 3. Preparamos el viaje hacia el LoginActivity
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+
+            // 4. Cerramos esta pantalla para que no consuma memoria
+            finish()
+        }
+    }
+}
